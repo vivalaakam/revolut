42fa9df8ad748ef665bb79970bfcc9021f97e530
Andrey Makarov <vivalaakam@gmail.com>
2016-09-27 10:23:22 +0300

    fix firefox bug

--- client/index.js
+++ client/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles/main.scss';
+import style from  './styles/main.scss';
 import 'normalize.css';
 import {configureStore} from './store/configureStore';
 import Root from './containers/Root';
 
 const store = configureStore();
 
+const root = document.getElementById('root')
+
+root.classList.add(style.root);
+
 ReactDOM.render(
     <Root store={store}/>,
-    document.getElementById('root')
+    root
 );
